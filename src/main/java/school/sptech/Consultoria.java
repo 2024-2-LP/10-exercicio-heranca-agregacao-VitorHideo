daa39fc52bb3edd6c1a4a12a2c7575916f45612e
VitorHideo <vitor.frutuoso@sptech.school>
2024-10-31 12:28:42 -0300

    exercicio consultoria

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,29 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+
+  private String nome;
+  private Integer vagas;
+  private List<Desenvolvedor> desenvolvedores;
+
+
+  public Consultoria(String nome, Integer vagas , List desenvolvedores) {
+    this.nome = nome;
+    this.vagas = vagas;
+    this.desenvolvedores = new ArrayList<>();
+  }
+
+  public Consultoria() {
+  }
+
+  public Boolean contratar(Desenvolvedor desenvolvedor) {
+    
+  }
+
+
+
+
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
@@ -1,4 +1,25 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorWeb {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorWeb extends Desenvolvedor {
+
+  private String backend;
+  private String frontend;
+  private String sgbd;
+  private Integer horasMentoria;
+
+  @Override
+  public Double calcularSalario() {
+    return super.calcularSalario() + (horasMentoria * 300);
+  }
+
+  public Boolean isFullstack() {
+    if (backend == null || frontend == null || sgbd == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
 }
